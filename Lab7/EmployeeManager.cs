75037c906c1d385ff6eec5290f968dc1b73b11ec
dung <dung@gmail.com>
2021-06-22 22:46:13 +0700

    update

---
+++ Lab7/EmployeeManager.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Lab7
+{
+    public class EmployeeManager
+    {
+        private Dictionary<string, Employee> _employees = new Dictionary<string, Employee>();
+        public void Create()
+        {
+            Console.WriteLine("Please enter ID: ");
+            var id = Console.ReadLine();
+            Console.WriteLine("Please enter employee name: ");
+            var name = Console.ReadLine();
+            Console.WriteLine("Please enter age: ");
+            var age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter salary: ");
+            var salary = double.Parse(Console.ReadLine());
+            _employees.Add(id,new Employee()
+            {
+                Name = name,
+                Age = age,
+                Salary = salary
+            });
+        }
+        public void Display ()
+        {
+            foreach (var list in _employees)
+            {
+                Console.WriteLine($"ID: {list.Key},{list.Value}");
+            }
+        }
+        public void Modify()
+        {
+            Console.WriteLine("Please enter ID: ");
+            var id = Console.ReadLine();
+            if (_employees.ContainsKey(id))
+            {
+                Console.WriteLine("Change the employee name to: ");
+                var name = Console.ReadLine();
+                Console.WriteLine("Change employee age to: ");
+                var age = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Change employee salary to: ");
+                var salary = double.Parse(Console.ReadLine());
+                _employees[id].Name = name;
+                _employees[id].Age = age;
+                _employees[id].Salary = salary;
+            }
+            else
+            {
+                Console.WriteLine("You must enter the employee ID!");
+            }
+        }
+        public void Delete()
+        {
+            Console.WriteLine("Please enter ID: ");
+            var id = Console.ReadLine();
+            if (_employees.ContainsKey(id))
+            {
+                _employees.Remove(id);
+            }
+            else
+            {
+                Console.WriteLine("You must enter the employee ID!");
+            }
+        }
+    }
+}
---
+++ Lab7/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Lab7
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var employeeManager = new EmployeeManager();
+            while (true)
+            {
+                Console.WriteLine("---------------------------------------");
+                Console.WriteLine("1. Add new employees.");
+                Console.WriteLine("2. Edit employees list.");
+                Console.WriteLine("3. Remove employees.");
+                Console.WriteLine("4. Show list of employees.");
+                Console.WriteLine("---------------------------------------");
+                Console.WriteLine("Please enter options (1-4): ");
+                var choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        employeeManager.Create();
+                        break;
+                    case 2:
+                        employeeManager.Modify();
+                        break;
+                    case 3:
+                        employeeManager.Delete();
+                        break;
+                    case 4:
+                        employeeManager.Display();
+                        break;
+                }
+            }
+        }
+    }
+}
+
